94e79cee6b96f1f4d900eb4b2671c266e01b5b37
Krista Veske <krista@zone.ee>
2023-09-28 12:35:47 +0300

    Initial commit - upload files

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://zone.eu/" target="_blank">
+      <img src="../zone_profile_square.svg" class="logo" alt="Zone logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Hello World!" />
+</template>
+
+<style scoped>
+.logo {
+  width: 200px;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #EC0009);
+}
+</style>
